0157662b48fa94b269e7018a5d30b73e6214ede8
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2023-11-08 15:13:52 -0300

    Add files via upload

---
+++ src/main/java/com/servidor/servidor_java/ServidorJavaApplication.java
@@ -0,0 +1,13 @@
+package com.servidor.servidor_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servidor/servidor_java/ServidorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servidor.servidor_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
