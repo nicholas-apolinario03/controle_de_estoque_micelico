1f07293f63a1f25c005b3ba5e8023c6ff8f5b856
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2023-11-22 10:54:16 -0300

    Delete src/main/java/com/trab/controle_de_estoque_micelico/repository directory

--- src/main/java/com/trab/controle_de_estoque_micelico/repository/CogumeloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trab.controle_de_estoque_micelico.repository;
-
-import com.trab.controle_de_estoque_micelico.Cogumelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CogumeloRepository extends JpaRepository<Cogumelo, Long> {
-}
